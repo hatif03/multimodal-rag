78dae7473ed9552d8c68d03b224bf0794004cdc7
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2025-03-07 19:16:28 +0530

    Adding function to save images locally

--- multimodal_rag.py
+++ multimodal_rag.py
@@ -19,3 +19,19 @@ def show_image_from_uri(uri):
     plt.show()
 
 
+dataset_folder = "./dataset/flowers"
+os.makedirs(dataset_folder, exist_ok=True)
+
+
+def save_images(dataset, dataset_folder, num_images=1000):
+    for i in range(num_images):
+        print(f"Saving image {i+1} of {num_images}")
+        # get image
+        image = dataset["train"][i]["image"]
+        # save image
+        image.save(os.path.join(dataset_folder, f"flower_{i+1}.png"))
+    
+    print(f"Saved the first 1000 images to : {dataset_folder}")
+
+
+save_images(ds, dataset_folder, num_images=1000)
