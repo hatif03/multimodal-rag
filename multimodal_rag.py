b87b255a82fe15e8d86079c1415dd6d3544f3342
Mohammad Hatif Osmani <66199079+hatif03@users.noreply.github.com>
2025-03-07 18:28:48 +0530

    adding image viewing function

--- multimodal_rag.py
+++ multimodal_rag.py
@@ -1,7 +1,21 @@
 from dotenv import load_dotenv
 
+from matplotlib import pyplot as plt
+import os
+from PIL import Image
+
+load_dotenv()
+
 from datasets import load_dataset
 
 ds = load_dataset("huggan/flowers-102-categories")
 
 print(ds.num_rows)
+
+def show_image_from_uri(uri):
+    img = Image.open(uri)
+    plt.imshow(img)
+    plt.axis("off")
+    plt.show()
+
+
